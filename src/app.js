6087a1e3562d319891578c3b3dff6a7d0d066813
rennon21 <95376888+rennon21@users.noreply.github.com>
2025-07-18 18:56:13 +0000

    generador

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function GeneradorExcusas() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+ 
+let randomwhois = who[Math.floor(Math.random() * who.length)];
+let randomactions = action[Math.floor(Math.random() * action.length)];
+let randomwhatis = what[Math.floor(Math.random() * what.length)];
+let randomwhenis = when[Math.floor(Math.random() * when.length)];
+
+let excusas= `${randomwhois} ${randomactions} ${randomwhatis} ${randomwhenis}`
+
+document.getElementById("excusas").innerHTML= excusas;
+
+console.log( randomwhois, randomactions, randomwhatis, randomwhenis );
+
 };
